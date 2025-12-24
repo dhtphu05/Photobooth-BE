ff923d968e038c9f8eaa8ec58cde5d441bdf6d8e
Đoàn Hoàng Thiên Phú <dhtphu05@gmail.com>
2025-12-25 01:06:52 +0700

    feat: Add backend service to docker-compose, remove orval mergeSchema option, and introduce frontend integration guide.

--- orval.config.ts
+++ orval.config.ts
@@ -7,9 +7,7 @@ export default defineConfig({
             target: './src/api/endpoints',
             schemas: './src/api/model',
             client: 'react-query',
-            parserOptions: {
-                mergeSchema: true
-            }
+
         },
         input: {
             target: 'https://api-photobooth.lcdkhoacntt-dut.live/api/docs-json',
