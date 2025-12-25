c5765122cfd9b9a3c1e1afb9c0994e1ceaa76c9f
Đoàn Hoàng Thiên Phú <dhtphu05@gmail.com>
2025-12-25 09:41:16 +0700

    feat: Increase request body size limit to 50MB for JSON and URL-encoded requests.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
 
